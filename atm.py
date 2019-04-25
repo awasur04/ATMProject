4e4fc375043a5af1c553a16ecb5bb1298ca458ff
awasur04 <awasur04@yahoo.com>
2019-04-24 21:20:48 -0500

    couple fixes

--- atm.py
+++ atm.py
@@ -2,7 +2,7 @@ import os
 ErrorCount = 0
 Accounts = [{"AccountNumber":0, "Pin":0, "Type":"Account Type", "Social":0, "Balance":0, "LastTransaction":0, "Interest":0}]
 
-
+#Tested works
 def Main():
     print("ATM")
     MainChoice = int(input("Are you a customer or staff member? (1 for customer, 2 for staff): "))
@@ -14,7 +14,7 @@ def Main():
         ShowStaffWelcome()
     else:
         print("That was not an option.")
-
+#Tested Works
 def Load():
     CheckingsFile = open('checkings.txt', 'r')
     SavingsFile = open('savings.txt', 'r')
@@ -49,7 +49,7 @@ def Load():
     CheckingsFile.close()
     SavingsFile.close()
     Main()
-
+#tested works
 def ShowCustomerWelcome(error):
     global ErrorCount
     if error == 0:
@@ -66,16 +66,15 @@ def ShowCustomerWelcome(error):
     elif ErrorCount >= 3:
         print("Invalid account information, returning to main menu")
         Main()
-
+#Tested Works
 def ShowStaffWelcome():
     Run = 'y'
     while Run == 'y':
         print("")
         print("Welcome to the staff home page")
-        print("1. Open Account for Checking")
-        print("2. Open Account for Savings")
-        print("3. Close Account")
-        print("4. Add Interest")
+        print("1. Open Account")
+        print("2. Close Account")
+        print("3. Interest Management")
         print("0. Log Out")
         Selection = int(input("What would you like to do?"))
         if Selection < 4 and Selection > 0:
@@ -86,8 +85,8 @@ def ShowStaffWelcome():
             Save()
         else:
             print("That was not an option.")
-            main()
-
+            Main()
+#tested works
 def Validate(type, TheAccount, ThePin):
     Found = 0
     if type == 1: #Customer accounts validation
@@ -101,15 +100,13 @@ def Validate(type, TheAccount, ThePin):
                     ShowCustomerWelcome(1)
         if Found == 0:
             ShowCustomerWelcome(1)
-
-    elif type == 2: #Staff account validation
-        print("hi")
-
+#Tested Works
 def ShowCustomerHome(TheAccount):
     for Account in Accounts:
         if Account["AccountNumber"] == TheAccount:
             if Account["Type"] == "Checkings": #Checkings account
                 Run = 'y'
+                OpenAccount = Account["AccountNumber"]
                 while Run == 'y':
                     print("")
                     print("Welcome to the home page")
@@ -124,8 +121,9 @@ def ShowCustomerHome(TheAccount):
                     Selection = int(input("What would you like to do?"))
                     if Selection < 6 and Selection > 0:
                         Run = 'N'
-                        BankFunction(Selection, TheAccount)
+                        BankFunction(Selection, OpenAccount)
                     elif Selection == 0:
+                        print("Have a nice day!")
                         Run = "N"
                         Save()
                     else:
@@ -133,6 +131,7 @@ def ShowCustomerHome(TheAccount):
 
             elif Account["Type"] == "Savings": #Savings Account
                 Run = 'y'
+                OpenAccount = Account["AccountNumber"]
                 while Run == 'y':
                     print("")
                     print("Welcome to the home page")
@@ -148,189 +147,185 @@ def ShowCustomerHome(TheAccount):
                     Selection = int(input("What would you like to do?"))
                     if Selection < 6 and Selection > 0:
                         Run = 'N'
-                        BankFunction(Selection, TheAccount)
+                        BankFunction(Selection, OpenAccount)
                     elif Selection == 0:
+                        print("Have a nice day!")
                         Run = "N"
                         Save()
                     else:
                         print("That was not an option.")
 
 def StaffFunction(option):
-    if option == 1: # Open Account for Checking
-        for Account in Accounts:
-            if Account["Type"] == "Checkings":
-                index = (Accounts[len(Accounts)-1])
-                LastAccount = Accounts[index]
-                for Items in LastAccount:
-                    LastAccountNumber = Items["AccountNumber"]
-                    NewAccountNumber = int(LastAccountNumber) + 1
-                PIN = (input("Enter in PIN: "))
-                if len(PIN) > 4 or len(PIN) < 4:
-                    print("ERROR!")
-                    print("PIN is invalid!")
-                    print("Enter in new PIN")
-                    return StaffFunction(1)
-                else:
-                    PIN = int(PIN)
-                SSN = (input("Enter in SSN: "))
-                if len(SSN) > 9 or len(SSN) < 9:
-                    print("ERROR!")
-                    print("SSN is invalid!")
-                    print("Enter in new SSN")
-                    return StaffFunction(1)
-                else:
-                    SSN = int(SSN)
-                Deposit = float(input("Enter in Deposit Amount: "))
-                AccountNumber = NewAccountNumber
-                Accounts.append({"AccountNumber":AccountNumber, "Pin":PIN, "Type":"Checkings", "Social":SSN, "Balance":Deposit, "LastTransaction":0, "Interest":0})
-                print("Account Number",AccountNumber)
-                print("Deposit Amount",Deposit)
-                print("Checkings Account Created")
-                ShowStaffWelcome()
-    elif option == 2:
-        for Account in Accounts:
-            if Account["Type"] == "Savings":
-                index = (Accounts[len(Accounts)-1])
-                LastAccount = Accounts[index]
-                for Items in LastAccount:
-                    LastAccountNumber = Items["AccountNumber"]
-                    NewAccountNumber = int(LastAccountNumber) + 1
-                PIN = (input("Enter in PIN: "))
-                if len(PIN) > 4 or len(PIN) < 4:
-                    print("ERROR!")
-                    print("PIN is invalid!")
-                    print("Enter in new PIN")
-                    return StaffFunction(1)
-                else:
-                    PIN = int(PIN)
-                SSN = (input("Enter in SSN: "))
-                if len(SSN) > 9 or len(SSN) < 9:
-                    print("ERROR!")
-                    print("SSN is invalid!")
-                    print("Enter in new SSN")
-                    return StaffFunction(1)
-                else:
-                    SSN = int(SSN)
-                INTEREST = input("Enter the amount of interest on the account: ")
-                Deposit = float(input("Enter in Deposit Amount: "))
-                AccountNumber = NewAccountNumber
-                Accounts.append({"AccountNumber":AccountNumber, "Pin":PIN, "Type":"Savings", "Social":SSN, "Balance":Deposit, "LastTransaction":0, "Interest":INTEREST})
-                print("Account Number",AccountNumber)
-                print("Deposit Amount",Deposit)
-                print("Savings Account Created")
-                ShowStaffWelcome()
-    elif option == 3:
-        #Doesnt Work Yet
+    if option == 1: # Open Account
+        TypeOfAccount = input("What kind of account would you like to open? (c or s)")
+        if TypeOfAccount.lower() == "c":
+            PIN = input("Please enter the PIN: ")
+            if len(PIN) > 4 or len(PIN) < 4:
+                print("ERROR!")
+                print("PIN is invalid!")
+                return StaffFunction(1)
+            else:
+                PIN = int(PIN)
+            SSN = (input("Enter in the Social Security Number: "))
+            if len(SSN) > 9 or len(SSN) < 9:
+                print("ERROR!")
+                print("SSN is invalid!")
+                return StaffFunction(1)
+            else:
+                SSN = int(SSN)
+            Deposit = float(input("Enter in Deposit Amount: "))
+
+            Accounts.append({"AccountNumber":AccountNumber, "Pin":PIN, "Type":"Checkings", "Social":SSN, "Balance":Deposit, "LastTransaction":0, "Interest":0})
+            print("Account Number",AccountNumber)
+            print("Deposit Amount",Deposit)
+            print("Checkings Account Created")
+            Selection = None
+            ShowStaffWelcome()
+        elif TypeOfAccount.lower() == "s":
+            print("hi")
+        else:
+            print("That was not a selection")
+            Selection = None
+            ShowStaffWelcome()
+    elif option == 2: #Close Account WORKS
         Found = 0
-        AccountToDelete = input("Enter account number to close: ")
+        AccountToDelete = input("Please input the account number you would like to delete: ")
         for Account in Accounts:
-            Length = len(Accounts)
             if Account["AccountNumber"] == AccountToDelete:
                 Found = 1
-                Confirm = input("Are you sure you want to delete account (", Account["AccountNumber"] "(y or n)")
-                if lower(Confirm) == "y":
-                    Accounts.remove()
-    elif option == 4:
+                Confirm = input("Are you sure you want to delete the account" + Account["AccountNumber"] + "? (y or n)")
+                if Confirm.lower() == "y":
+                    Accounts.remove(Account)
+                    print("Account Removed.")
+                    print(Accounts)
+                    Selection = None
+                    ShowStaffWelcome()
+                elif Confirm.lower() == 'n':
+                    print("Returning to main menu.")
+                    Selection = None
+                    ShowStaffWelcome()
+        if Found == 0:
+            print("Account not found.")
+            Selection = None
+            ShowStaffWelcome()
+    elif option == 3: #Interest WORKS
         Found = 0
         AccountToChange = input("Enter account number to add interest to: ")
-        for Accounts in Acocunt:
+        for Account in Accounts:
             if Account["AccountNumber"] == AccountToChange:
                 Found = 1
                 print("Current interest on account is %", Account["Interest"], sep='')
-                InterestToAdd = int(input("Enter amount of interest to add: "))
-                Account["Interest"] = int(Account["Interest"]) + InterestToAdd
-                print("Interest Added to account.")
-                ShowStaffWelcome()
+                Choice = input("Would you like to add or remove interest?")
+                if Choice == "add":
+                    InterestToAdd = int(input("Enter amount of interest to add: "))
+                    Account["Interest"] = int(Account["Interest"]) + InterestToAdd
+                    print("Interest Added to account.")
+                elif Choice == "remove":
+                    InterestToRemove = int(input("Enter amount of interest to remove: "))
+                    if int(Account["Interest"]) >= InterestToRemove:
+                        Account["Interest"] = int(Account["Interest"]) - InterestToRemove
+                        print("Interest Removed from account.")
+                    else:
+                        print("ERROR: Unable to remove that amount.")
+        Selection = None         
+        ShowStaffWelcome()
         if Found == 0:
             print("Account not found please try again")
+            Selection = None
             ShowStaffWelcome()
 
-
-def BankFunction(option, Account):
+#TESTED WORKS
+def BankFunction(option, OpenAccount):
     for Account in Accounts:
-        if Account["AccountNumber"] == Account:
+        if Account["AccountNumber"] == OpenAccount:
             if option == 1: #Withdraw
                 AmountToWithdraw = float(input("How much would you like to withdraw? "))
                 if AmountToWithdraw > float(Account["Balance"]):
                     print("ERROR!")
                     print("You only have $", Account["Balance"], " in your account", sep='')
                     Selection = None
-                    ShowCustomerHome(Account)
+                    ShowCustomerHome(OpenAccount)
                 elif AmountToWithdraw < float(Account["Balance"]):
                     Account["Balance"] = float(Account["Balance"]) - AmountToWithdraw
                     print("You have withdrawn $", AmountToWithdraw, sep='')
                     print("Your new account balance is $", Account["Balance"], sep='')
                     Selection = None
-                    ShowCustomerHome(Account)
+                    ShowCustomerHome(OpenAccount)
             elif option == 2: #Deposit
                 AmountToDeposit = float(input("How much would you like to deposit? "))
                 Account["Balance"] = float(Account["Balance"]) + AmountToDeposit
                 print("Your new account balance is $", Account["Balance"], sep='')
                 Account["LastTransaction"] = AmountToDeposit
                 Selection = None
-                ShowCustomerHome(Account)
+                ShowCustomerHome(OpenAccount)
             elif option == 3: #Balance
                 print("Your account balance is $", Account["Balance"], sep='')
                 Selection = None
-                ShowCustomerHome(Account)
+                ShowCustomerHome(OpenAccount)
             elif option == 4: #Transfer
                 AmountToTransfer = float(input("Enter amount to transfer: "))
-                AccountToTransfer = input("Enter the account number to trasnfer to: ")
-                if AmountToTransfer > Account["Balance"]:
+                AccountToTransfer = input("Enter the account number to transfer to: ")
+                if AmountToTransfer > float(Account["Balance"]):
                     print("Error")
                     print("You only have $", Account["Balance"], " in your account", sep='')
                     Selection = None
-                    ShowCustomerHome(Account)
+                    ShowCustomerHome(OpenAccount)
                 else:
-                    Account["Balance"] = float(Account["Balance"]) - AmountToTransfer
-                    Transfer(AccountToTransfer, AmountToTransfer, Account)
+                    if AccountToTransfer == OpenAccount:
+                        print("ERROR: Cant transfer to your own account.")
+                        Selection = None
+                        ShowCustomerHome(OpenAccount)
+                    else:
+                        Account["Balance"] = float(Account["Balance"]) - AmountToTransfer
+                        Transfer(AccountToTransfer, AmountToTransfer, OpenAccount)
             elif option == 5: #Last Deposit Amount
                 print("Your last deposit amount was $", Account["LastTransaction"], sep='')
                 Selection = None
-                ShowCustomerHome(Account)
-
+                ShowCustomerHome(OpenAccount)
+#TESTED WORKS
 def Transfer(Target, Amount, Account):
+    Found = 0
     if Target and Amount:
+        MainAccount = Account
         for Account in Accounts:
             if Account["AccountNumber"] == Target:
+                Found = 1
                 Account["Balance"] = float(Account["Balance"]) + Amount
                 print("Transfer Successful")
                 Selection = None
-                ShowCustomerHome(Account)
-            else:
-                print("Sorry, that account was not found.")
-                Selection = None
-                ShowCustomerHome(Account)
+                ShowCustomerHome(MainAccount)
+    if Found == 0:
+        MainAccount = Account
+        print("Sorry, that account was not found.")
+        Selection = None
+        ShowCustomerHome(MainAccount)
 
 
-
+#Tested WORKS
 def Save():
     Type = None
-    TempFile = open("temp.txt", 'w')
+    TempFileCheckings = open("tempcheck.txt", 'w')
+    TempFileSavings = open("tempsavings.txt", 'w')
     for Account in Accounts:
         if Account["Type"] == "Checkings": #Checkings
-            Type = "Checkings"
-            TempFile.write(Account["AccountNumber"] + '\n')
-            TempFile.write(Account["Pin"] + '\n')
-            TempFile.write(Account["Social"] + '\n')
-            TempFile.write(Account["Balance"] + '\n')
-            TempFile.write(Account["LastTransaction"] + '\n')
+            TempFileCheckings.write(Account["AccountNumber"] + '\n')
+            TempFileCheckings.write(Account["Pin"] + '\n')
+            TempFileCheckings.write(Account["Social"] + '\n')
+            TempFileCheckings.write(str(Account["Balance"]) + '\n')
+            TempFileCheckings.write(str(Account["LastTransaction"]) + '\n')
         elif Account["Type"] == "Savings": #Savings
-            Type = "Savings"
-            TempFile.write(Account["AccountNumber"] + '\n')
-            TempFile.write(Account["Pin"] + '\n')
-            TempFile.write(Account["Social"] + '\n')
-            TempFile.write(Account["Balance"] + '\n')
-            TempFile.write(Account["LastTransaction"] + '\n')
-            TempFile.write(Account["Interest"] + '\n')
-    TempFile.close()
-    if Type == "Checkings":
-        os.remove("checkings.txt")
-        os.rename("temp.txt", 'checkings.txt')
-        print("File Saved")
-    elif Type == "Savings":
-        os.remove("savings.txt")
-        os.rename("temp.txt", 'savings.txt')
-        print("File Saved")
+            TempFileSavings.write(Account["AccountNumber"] + '\n')
+            TempFileSavings.write(Account["Pin"] + '\n')
+            TempFileSavings.write(Account["Social"] + '\n')
+            TempFileSavings.write(str(Account["Balance"]) + '\n')
+            TempFileSavings.write(str(Account["LastTransaction"]) + '\n')
+            TempFileSavings.write(str(Account["Interest"]) + '\n')
+    TempFileCheckings.close()
+    TempFileSavings.close()
+    os.remove("checkings.txt")
+    os.rename("tempcheck.txt", 'checkings.txt')
+    os.remove("savings.txt")
+    os.rename("tempsavings.txt", 'savings.txt')
+    print("File Saved")
 
 Load()
